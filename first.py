109ffa74ce0bc5f9ca2ec33eb7b5a08954a63dda
Risahdkhan <rishadkhan670@gmail.com>
2021-08-31 12:21:15 +0600

    Change a more feature

--- first.py
+++ first.py
@@ -7,21 +7,3 @@ elif n == 3:
     print('March')
 elif n == 4:
     print('April')
-elif n == 5:
-    print('May')
-elif n == 6:
-    print('June')
-elif n == 7:
-    print('July')
-elif n == 8:
-    print('August')
-elif n == 9:
-    print('September')
-elif n == 10:
-    print('October')
-elif n == 11:
-    print('November')
-elif n == 12:
-    print('December')
-
-print('Rishad Khan')
