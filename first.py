f52cde5921ad0271ef5a38ec595058e6f95b68f6
Risahdkhan <rishadkhan670@gmail.com>
2021-08-31 15:33:45 +0600

    This side is checkout

--- first.py
+++ first.py
@@ -1,24 +1,9 @@
 n = int(input())
 if n == 1:
     print('January')
-elif n == 2:
+elif n == 2
     print('February')
 elif n == 3:
     print('March')
 elif n == 4:
     print('April')
-<<<<<<< HEAD
-elif n == 5:
-    print('May')
-elif n == 6:
-    print('June')
-elif n == 7:
-    print('July')
-elif n == 8:
-    print('August')
-elif n == 9:
-    print('September')
-elif n == 10:
-    print('October')
-=======
->>>>>>> new-developer
