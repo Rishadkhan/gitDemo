ed99a2e03445cf6b0770e8b1dd62392c2251810d
Risahdkhan <rishadkhan670@gmail.com>
2021-09-06 11:28:41 +0600

    This commit is to need

--- first.py
+++ first.py
@@ -7,3 +7,13 @@ elif n == 3:
     print('March')
 elif n == 4:
     print('April')
+elif n == 5:
+    print('May')
+elif n == 6:
+    print('June')
+elif n == 7:
+    print('July')
+elif n == 8:
+    print('August')
+elif n == 9:
+    print('September')
